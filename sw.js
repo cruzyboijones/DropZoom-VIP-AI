ce8bbfd7dd5e5f853cdeda441fd19d3012cf49ce
cruzyboijones <cruzejones@icloud.com>
2025-06-08 10:24:31 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('dz-cache').then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      '/ai-helper.html',
+      '/explore.html',
+      '/style.css'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
 
